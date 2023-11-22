0f479a6d736c7498a5a3949170722b923ab83699
ntnam2002 <131666240+ntnam2002@users.noreply.github.com>
2023-11-22 19:02:05 +0700

    sth

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "picsum.photos",
+            "loremflickr.com",
+            "placeimg.com",
+            "source.unsplash.com",
+        ],
+    },
+};
